627f6e0d927273aa0abdde091e0c8684ecf50df0
Omar Flores <floresquispeomar8@gmail.com>
2025-06-03 09:18:27 -0500

    correccion de estilos y adiccion de alertas

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -2,18 +2,23 @@ const connection = require('../config/db'); // Asegúrate de tener la conexión 
 
 // Operación de Creación (Create):
 exports.crearProducto = (req, res) => {
-    const producto = {
+    const nuevoProducto = {
         nombre: req.body.nombre,
         precio: req.body.precio,
         stock: req.body.stock,
-        imagen: req.file ? req.file.filename : ''
+        imagen: req.file ? req.file.filename : null
     };
-    const query = "INSERT INTO productos SET ?";
-    connection.query(query, producto, (err, result) => {
+
+    connection.query("INSERT INTO productos SET ?", nuevoProducto, (err, result) => {
         if (err) throw err;
-        res.send('Producto creado exitosamente');
+        res.render('productos/crear', {
+            mensaje: 'Producto creado exitosamente'
+        });
     });
 };
+
+
+
 // Operación de Edicion (Edit):
 exports.editarProducto = (req, res) => {
     const id = req.params.id;
@@ -55,7 +60,14 @@ exports.actualizarProducto = (req, res) => {
     };
     connection.query("UPDATE productos SET ? WHERE id = ?", [productoActualizado, id], (err, result) => {
         if (err) throw err;
-        res.send('Producto actualizado exitosamente');
+        // Obtener el producto actualizado para mostrarlo en el formulario
+        connection.query("SELECT * FROM productos WHERE id = ?", [id], (err2, results) => {
+            if (err2) throw err2;
+            res.render('productos/editar', { 
+                producto: results[0],
+                mensaje: 'Producto actualizado exitosamente'
+            });
+        });
     });
 };
 
